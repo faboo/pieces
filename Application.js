b1e603b44fa413a58b4fdc0fb9c932ad120d0ce8
Ray Wallace <the.faboo@gmail.com>
2012-06-10 14:43:41 -0400

    Copyright headers
    Copyright headers, a license, and a readme.

--- Application.js
+++ Application.js
@@ -1,3 +1,18 @@
+//Pieces, Copyright (C) 2012 Ray Wallace
+//
+//This program is free software; you can redistribute it and/or modify it under
+//the terms of the GNU General Public License as published by the Free Software
+//Foundation version 2 of the Licens.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT
+//ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+//FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//details.
+//
+//You should have received a copy of the GNU General Public License along with
+//this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+//Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 function Application(base){
 	if(Application.current !== null)
 		throw new Error("Only one Application can run at a time");
@@ -12,19 +27,3 @@ Application.prototype = new Widget();
 Widget.autoWidget['application'] = Application;
 
 Application.prototype.templateName = "#application-template";
-/*
-Application.prototype.onSave = function onSave(){
-	var data = this.editor.scene().serialize();
-
-	window.localStorage.setItem("edit-image", data);
-}
-
-Application.prototype.onLoad = function onLoad(){
-	var data = window.localStorage.getItem("edit-image");
-
-	this.editor.load(data);
-}
-
-Application.prototype.onNew = function onNew(){
-	this.editor.load(new Scene());
-}*/
